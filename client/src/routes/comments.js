0091127db931369a3ee4e286002a1ff87e10bd12
odelavia <odelavia@gmail.com>
2018-05-25 04:10:33 -0400

    set up comment ownership check for edit and delete routes

--- client/src/routes/comments.js
+++ client/src/routes/comments.js
@@ -39,7 +39,7 @@ router.post('/', isLoggedIn, (req, res) => {
 });
 
 //EDIT route
-router.get("/:comment_id/edit", (req, res) => {
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
   Comment.findById(req.params.comment_id, (err, foundComment) => {
     if (err) {
       res.redirect("back")
@@ -50,7 +50,7 @@ router.get("/:comment_id/edit", (req, res) => {
 });
 
 //UPDATE route
-router.put("/:comment_id", (req, res) => {
+router.put("/:comment_id", checkCommentOwnership, (req, res) => {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
     if (err) {
       res.redirect("back");
@@ -61,7 +61,7 @@ router.put("/:comment_id", (req, res) => {
 })
 
 //DESTROY route
-router.delete("/:comment_id", (req, res) => {
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
   Comment.findByIdAndRemove(req.params.comment_id, (err) => {
     if (err) {
       res.redirect("back");
@@ -77,6 +77,24 @@ function isLoggedIn(req, res, next) {
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+      if (err) {
+        res.redirect("back");
+      } else {
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
